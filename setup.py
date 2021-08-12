aaac54b6150e6dfa71dee4c44a5439d18b8a4735
danx0r <dan.miller@eye0.com>
2021-08-11 17:26:37 -0700

    debug pip problem

--- setup.py
+++ setup.py
@@ -1,24 +1,11 @@
-print("LEELALIB SETUP.PY")
-
 from setuptools import setup, find_packages
-
-import os, sys
-print("PATH", sys.path)
-print("CWD", os.getcwd())
-os.system('ls -la "' + os.getcwd() + '"')
-
-from leelalib.version import version
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="leelalib",
-    version=version,
-    py_modules=['config'],
-    packages=find_packages(where="leelalib"),
-    python_requires='>=3.6',
-    scripts=[],
+    version="0.0.0b1",
+#    py_modules=['config'],
+    packages=find_packages(),
+    python_requires='>=3',
+#    scripts=[],
     install_requires=[
         "PyYAML             >=5.3.1,    <6",
     ],
